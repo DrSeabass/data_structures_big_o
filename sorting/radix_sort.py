6bc8572ee883f7bc68c6fda420b9e5cb0c546137
Jordan Thayer <jtthayer@sep.com>
2024-12-02 14:24:13 -0500

    Pull radix sort into separate file

---
+++ sorting/radix_sort.py
@@ -0,0 +1,29 @@
+def counting_sort(input, radix):
+    buckets = [[], [], [], [], [], [], [], [], [], [],] # one for each digit in 0 - 9
+    # Bucketize input based on radix
+    saw_non_zero = False
+    for el in input:
+        bucket = el % (radix * 10) // radix
+        saw_non_zero = saw_non_zero or bucket > 0
+        buckets[bucket].append(el)
+    
+    # reconstruct output from buckets
+    output = []
+    for bucket in buckets:
+        output.extend(bucket)
+    return output, saw_non_zero
+
+
+def radix_sort(input):
+    radix = 1
+    finished = False
+    next_list = input
+    while not finished:
+        next_list, saw_non_zero = counting_sort(next_list, radix)
+        radix *= 10
+        finished = not saw_non_zero
+    # The other sorts are destructive / in-place
+    # Our implementation of counting sort doesn't allow for this
+    # We overwrite the original input to get matching destructive behavior
+    for i in range(len(input)):
+        input[i] = next_list[i]
--- sorting/sorting_algorithms.py
+++ sorting/sorting_algorithms.py
@@ -75,6 +75,7 @@ class Split():
     
     def __str__(self):
         return f"[{self.start}, {self.end}]"
+    
 
 def merge_sort(input, split=None, comparitor=default_comparitor):
     if split is None:
@@ -85,39 +86,7 @@ def merge_sort(input, split=None, comparitor=default_comparitor):
     merge_sort(input, split=left, comparitor=comparitor)
     merge_sort(input, split=right, comparitor=comparitor)
     Split.merge(left, right, input_list=input, comparitor=comparitor)
-    
 
-
-def counting_sort(input, radix):
-    buckets = [[], [], [], [], [], [], [], [], [], [],] # one for each digit in 0 - 9
-    # Bucketize input based on radix
-    saw_non_zero = False
-    for el in input:
-        bucket = el % (radix * 10) // radix
-        saw_non_zero = saw_non_zero or bucket > 0
-        buckets[bucket].append(el)
-    
-    # reconstruct output from buckets
-    output = []
-    for bucket in buckets:
-        output.extend(bucket)
-    return output, saw_non_zero
-
-
-def radix_sort(input):
-    radix = 1
-    finished = False
-    next_list = input
-    while not finished:
-        next_list, saw_non_zero = counting_sort(next_list, radix)
-        radix *= 10
-        finished = not saw_non_zero
-    # The other sorts are destructive / in-place
-    # Our implementation of counting sort doesn't allow for this
-    # We overwrite the original input to get matching destructive behavior
-    for i in range(len(input)):
-        input[i] = next_list[i]
-    
 
 if __name__ == "__main__":
     make_example_inputs()
