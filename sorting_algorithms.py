854456f672ea7857e8fcf0323cf802ff6f02439c
Jordan Thayer <jtthayer@sep.com>
2024-12-02 10:37:13 -0500

    Implements bubble sort

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -39,7 +39,21 @@ def bogo_sort(input, comparitor=default_comparitor):
 
 
 def bubble_sort(input, comparitor=default_comparitor):
-    raise NotImplemented
+    items = len(input)
+    if items == 0:
+        return
+    sorted_this_pass = False
+    while not sorted_this_pass:
+        prev = 0
+        sorted_this_pass = True
+        for i in range(1, items):
+            tmp_a = input[prev]
+            tmp_b = input[i]
+            if not comparitor(tmp_a, tmp_b):
+                input[prev] = tmp_b
+                input[i] = tmp_a
+                sorted_this_pass = False
+            prev = i
 
 
 def merge_sort(input, comparitor=default_comparitor):
