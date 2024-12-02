296f5d5a33f4c7c5aa2c91cdb1285ee0e145c262
Jordan Thayer <jtthayer@sep.com>
2024-12-02 15:16:14 -0500

    Set up sorting algs for input size study

--- sorting/sorting_algorithms.py
+++ sorting/sorting_algorithms.py
@@ -7,6 +7,7 @@ from radix_sort import radix_sort
 DEFAULT_SIZE = 10
 MAX_INT = 10000
 MIN_INT = 0
+DEFAULT_SIZE_SCHEDULE = [1, 3, 5, 10, 100, 500, 1000, 100000]
 sorted_data = None
 reversed_order = None
 shuffled = None
@@ -46,6 +47,27 @@ def make_example_inputs():
     random_values = make_random_input(samples=1)[0]
 
 
+def generate_count(sort_alg, input):
+    reset_counter()
+    sort_alg(input, default_comparator)
+    return comparison_calls
+    
+
+def generate_dataset(sort_algs, size_sched=DEFAULT_SIZE_SCHEDULE, samples_per_size=1):
+    results = {}
+    for size in size_sched:
+        inputs = make_random_input(size=size, samples=samples_per_size)
+        for alg in sort_algs:
+            if alg.__name__ not in results:
+                results[alg.__name__] = {}
+            if size not in results[alg.__name__]:
+                results[alg.__name__][size] = list(map(
+                    lambda input: generate_count(alg, input),
+                    inputs
+                ))
+    return results
+
+
 if __name__ == "__main__":
     make_example_inputs()
     print("sorted  :", sorted_data, is_sorted(sorted_data, default_comparator))
@@ -54,9 +76,15 @@ if __name__ == "__main__":
     print("empty   :", empty, is_sorted(empty, default_comparator))
     reset_counter()
 
-    print(random_values)
+    # Some Templates for 1sie 2sie testing
+    #print(random_values)
     #bogo_sort(random_values, default_comparator)
     #bubble_sort(random_values, default_comparator)
-    #radix_sort(random_values)
+    #radix_sort(random_values, default_comparator)
     #merge_sort(random_values, default_comparator)
-    print(random_values)
+    #print(random_values)
+
+    # Template for generating a series to look at scaling behavior
+    data = generate_dataset([merge_sort])
+    for alg, results in data.items():
+        print(alg, results)
