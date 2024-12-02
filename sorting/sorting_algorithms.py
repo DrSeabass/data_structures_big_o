a3742e6bcfc2336bb6b7ba24ce70b073fd33c805
Jordan Thayer <jtthayer@sep.com>
2024-12-02 14:16:33 -0500

    Pull bubble sort out into separate file

--- sorting/sorting_algorithms.py
+++ sorting/sorting_algorithms.py
@@ -43,23 +43,6 @@ def bogo_sort(input, comparitor=default_comparitor):
         shuffle(input)
 
 
-def bubble_sort(input, comparitor=default_comparitor):
-    items = len(input)
-    if items == 0:
-        return
-    sorted_this_pass = False
-    while not sorted_this_pass:
-        prev = 0
-        sorted_this_pass = True
-        for i in range(1, items):
-            tmp_a = input[prev]
-            tmp_b = input[i]
-            if not comparitor(tmp_a, tmp_b):
-                input[prev] = tmp_b
-                input[i] = tmp_a
-                sorted_this_pass = False
-            prev = i
-
 class Split():
     start = -1
     end = -1
