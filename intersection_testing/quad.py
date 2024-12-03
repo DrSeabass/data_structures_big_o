0b912054812fe5c812bbf4f52d11b58793b47cd0
Jordan Thayer <jtthayer@sep.com>
2024-12-03 10:18:51 -0500

    Pull quad out into separate file

---
+++ intersection_testing/quad.py
@@ -0,0 +1,39 @@
+from point import Point
+
+class Quad():
+    north = None
+    east = None
+    south = None
+    west = None
+    corners = None
+
+    def __init__(self, n, e, s, w):
+        self.north = n
+        self.east = e
+        self.south = s
+        self.west = w
+        self.corners = [Point(n,w), Point(n,e), Point(s,e), Point(s,w)]
+
+    def __str__(self):
+        return f"[({self.west}, {self.north}), ({self.east}, {self.north}), ({self.east}, {self.south}), ({self.west}, {self.south})]"
+    
+    def contains(self, p : Point) -> bool:
+        return (
+            p.x >= self.west and
+            p.x <= self.east and
+            p.y >= self.south and
+            p.y <= self.north
+        )
+    
+    def intersects(self, quad_2) -> bool:
+        return (
+            # self contains quad_2
+            (self.north >= quad_2.north and self.south <= quad_2.south and self.west <= quad_2.west and self.east >= quad_2.east) or
+            # quad_2 contains self
+            (self.north <= quad_2.north and self.south >= quad_2.south and self.west >= quad_2.west and self.east <= quad_2.east) or
+            # self contains one of the corners of quad_2 (previous cases handle abutment)
+            self.contains(quad_2.corners[0]) or
+            self.contains(quad_2.corners[1]) or
+            self.contains(quad_2.corners[2]) or
+            self.contains(quad_2.corners[3])
+        )
