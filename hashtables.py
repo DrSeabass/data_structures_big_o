906993130174ba1b5ec254859364ccb9d96fc486
Jordan Thayer <jtthayer@sep.com>
2024-12-04 08:08:48 -0500

    Wrap hash bucket items to hang key on them
    Could have done intrusive data structure instead, but lets teach one thing at a time

--- hashtables.py
+++ hashtables.py
@@ -1,13 +1,33 @@
-class Bucket():
-    def __init__(self, key=None):
-        self.contents = []
+class Container():
+    def __init__(self, key, element):
         self.key = key
+        self.element = element
 
-    def add(self, element):
-        raise NotImplemented()
+class Bucket():
+    def __init__(self):
+        self.contents = []
+
+    def add(self, key, element):
+        self.contents.append(Container(key, element))
     
-    def remove(self, element):
-        raise NotImplemented()
+    def remove(self, key, element):
+        new_contents = []
+        deleted_count = 0
+        for container in self.contents:
+            if container.key != key or container.element != element:
+                new_contents.append(container)
+            else:
+                deleted_count += 1
+        return deleted_count
+    
+    def find(self, key):
+        for container in self.contents:
+            if container.key == key:
+                return container.element
+        return None
+
+    def contains(self, key):
+        return self.find(key) is not None
 
 class HashTable():
 
@@ -18,13 +38,26 @@ class HashTable():
             self.buckets.append(Bucket())
         self.hash = hash_function
 
-    def contains(self, element):
-        raise NotImplemented()
+    def contains(self, key):
+        bucket = self.buckets[key % self.max_bucket]
+        return bucket.contains(key)
     
     def retrieve(self, key):
-        raise NotImplemented()
+        bucket = self.buckets[key % self.max_bucket]
+        return bucket.find(key)
     
-    def add(self, element):
+    def add(self, element, key=None):
+        if key is None:
+            key = self.hash(element)
+        bucket = self.buckets[key % self.max_bucket]
+        bucket.add(key, element)
+
+    def remove(self, element):
+        key = self.hash(element)
+        bucket = self.buckets[key % self.max_bucket]
+        bucket.remove(key, element)
+
+    def extend(self):
         raise NotImplemented()
 
 
